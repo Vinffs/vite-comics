541e4d235c7aad54571ce021e71cf53a3084b012
Vinffs <vin.demiranda@gmail.com>
2023-11-09 13:53:05 +0100

    created components and removed import base.css

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    QUESTO E' IL FOOTER
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'AppFooter',
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    QUESTO E' IL HEADER
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'AppHeader',
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
